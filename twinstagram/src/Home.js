ee109c7d3ac0c8506fdd6282cfcb838e9ad6ce4d
Fernando Aponte <faponte1230@yahoo.com>
2023-04-03 22:26:39 -0400

    edit

--- twinstagram/src/Home.js
+++ twinstagram/src/Home.js
@@ -18,7 +18,7 @@ function Home(){
                 To get started, click 'Get Started'. Then enter your twin information!<br></br>
                 The upload form will redirect you to the gallery, where all twins live!<br></br><br></br>
                 ~ Our Chat is still in beta ~<br></br></h4>
-                <p></p>
+                <strong></strong>
                 <button onClick={handleStart}>Get Started</button>
             </section> 
             
