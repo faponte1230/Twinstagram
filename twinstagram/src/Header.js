ea12b8a665ac953af828ae45fdf4c2e7c2b608fe
Fernando Aponte <faponte1230@yahoo.com>
2023-04-03 22:42:09 -0400

    edit

--- twinstagram/src/Header.js
+++ twinstagram/src/Header.js
@@ -3,9 +3,8 @@ import React from "react";
 function Header(){
     return (
         <header className="App-header">
-                <h1>TwiinstaGram</h1>
-                <h4>The Social Media Platform Exclusively For Twins!</h4><br></br>
-                
+            <h1>TwiinstaGram</h1>
+            <h4>The Social Media Platform Exclusively For Twins!</h4>
         </header>
     )
 }
