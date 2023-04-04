861deec6682f0e4285f55956e789e4e3cd182ba0
Fernando Aponte <faponte1230@yahoo.com>
2023-04-03 22:23:35 -0400

    edit

--- twinstagram/src/Home.js
+++ twinstagram/src/Home.js
@@ -14,7 +14,9 @@ function Home(){
         <div className="home">
             <section id='home'>
                 <h2> About TwiinstaGram</h2>
-                <h4>TwinstaGram is the newest social platform for users who have a double! </h4>
+                <h4>TwinstaGram is the newest social platform for users who have a double!<br></br>
+                To get started, click 'Get Started'. Then enter your twin information!<br></br>
+                The upload form will redirect you to the gallery, where all twins live!</h4>
                 <p></p>
                 <button onClick={handleStart}>Get Started</button>
             </section> 
